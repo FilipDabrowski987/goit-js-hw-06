f9d51df4242f661ea9dfad192fabe1facbfc6fba
FilipDabrowski987 <filipdabrowski@vp.pl>
2024-02-11 12:33:20 +0100

    update

--- src/js/task-08.js
+++ src/js/task-08.js
@@ -1,1 +1,22 @@
+const registerForm = document.querySelector('.login-form');
 
+registerForm.addEventListener('submit', submitFunction);
+
+function submitFunction(event) {
+  event.preventDefault();
+  const form = event.target;
+  const email = form.elements.email.value;
+  const password = form.elements.password.value;
+  
+  if (email === '' || password === '') {
+    return alert('Wszystkie pola powinny zostać wypełnione!');
+    };
+
+    let formData = {
+        email: email,
+        password: password,
+    };
+
+    console.log(formData);
+    form.reset();
+};
